d1b1987f5d5d15bc260f3de90995f13a8d681b7f
Katie <costantinkp@gmail.com>
2022-01-21 11:52:16 -0500

    initialize app and set up color box and color input components

---
+++ src/ColorBox.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ColorBox = () => {
+  return <div></div>;
+};
---
+++ src/ColorInput.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ColorInput = () => {
+  return <div></div>;
+};
